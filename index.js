e52cdd433d5160d00abda0b30621ba57674621f0
dimartvil <dimartvil@hotmail.com>
2020-11-16 02:18:35 -0600

    Se agregan estilos y contenido de la sección about

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const $MENU_BUTTON = document.getElementById('menu_button')
 const $MENU = document.querySelector('.menu')
+const $HERO = document.querySelector('.hero-filter')
+const $HEADER = document.querySelector('header')
 
 
-
+/*Función para controlar el botón del menú*/
 $MENU_BUTTON.addEventListener('click', ()=> {
     $MENU.classList.toggle('active')
 })
+
+
+/*Función para controlar la animación por scroll del menú*/
+
+const observerOptions = 0.25
+
+const observer = new IntersectionObserver(darkFixedMenu, {threshold: observerOptions})
+
+observer.observe($HERO)
+
+function darkFixedMenu(entries){
+    const entry = entries[0]
+    const target = $HEADER
+    const isVisible = entry.intersectionRatio >= observerOptions;
+        if(isVisible){
+            target.classList.remove("dark")
+            target.style.position = "inherit"
+        } else{
+            target.classList.add("dark")
+            target.style.position = "fixed"
+        }
+}
+
+
+
+
 
